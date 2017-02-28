7e2bd43c465c2a0ae0efad146dbb5081421105af
Justin Soliz <justinsoliz@gmail.com>
2017-02-28 15:32:47 -0800

    customize oh-my-zsh candy theme

---
+++ oh_my_zsh/load_oh_my_zsh.zsh
@@ -0,0 +1,24 @@
+
+# load in oh_my_zsh
+# this zsh file derived from template .oh-my-.zsh/templates/zshrc.zsh-template
+
+# Path to your oh-my-zsh installation 
+# export OH_MY_ZSH="$ZSH/oh_my_zsh/.oh-my-zsh"
+export ZSH=$DOTFILES/oh_my_zsh/.oh-my-zsh
+
+# set custom directory for themes and plugins
+# https://github.com/robbyrussell/oh-my-zsh/wiki/Customization
+export ZSH_CUSTOM=$DOTFILES/oh_my_zsh/custom
+
+# Set name of the theme to load. Optionally, if you set this to "random"
+# it'll load a random theme each time that oh-my-zsh is loaded.
+# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+ZSH_THEME="candy"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git)
+
+source $ZSH/oh-my-zsh.sh
