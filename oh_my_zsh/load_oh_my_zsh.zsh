ebd923f247a31962d9f6afa8cc9fcf24613a0d6e
Justin Soliz <justinsoliz@gmail.com>
2017-03-01 08:18:53 -0800

    remove git plugin from oh_my_zsh

--- oh_my_zsh/load_oh_my_zsh.zsh
+++ oh_my_zsh/load_oh_my_zsh.zsh
@@ -19,6 +19,6 @@ ZSH_THEME="candy"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+# plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
