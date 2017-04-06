1fce240e373d4ce090195f0471da0cc098358103
Justin Soliz <justinsoliz@gmail.com>
2017-04-05 19:10:39 -0700

    include git author and email

--- git/install.sh
+++ git/install.sh
@@ -2,3 +2,5 @@
 # git configuration setup
 git config --global push.default simple
 git config --global core.editor "vim"
+git config --global user.name "Justin Soliz"
+git config --global user.email justinsoliz@gmail.com
