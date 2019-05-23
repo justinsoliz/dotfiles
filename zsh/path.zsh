344f8323d0a499e38681cf4422b6b9ad6b250cca
Justin Soliz <justinsoliz@gmail.com>
2019-05-23 10:39:49 +0100

    adding visual studio code to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,5 +1,8 @@
 # zsh/path.zsh
 
 # mysql workbench tools
-export PATH=$PATH:/Applications/MySQLWorkbench.app/Contents/MacOS
+export PATH="$PATH:/Applications/MySQLWorkbench.app/Contents/MacOS"
+
+# Add Visual Studio Code (code)
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
