d0f306cc9c8d8b5a60c47d3669599bd8ee4cb9e2
Justin Soliz <justinsoliz@gmail.com>
2019-10-22 18:50:55 -0300

    who knows

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -6,3 +6,8 @@ export PATH="$PATH:/Applications/MySQLWorkbench.app/Contents/MacOS"
 # Add Visual Studio Code (code)
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
+# hashicorps
+export PATH="$PATH:$HOME/hashicorps/bin"
+
+# ruby gems
+export PATH="$PATH:/usr/local/lib/ruby/gems"
