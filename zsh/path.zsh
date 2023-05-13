6e97b5d2679f6c5aaf8ba3833ff5cde1694df65f
Justin Soliz <justinsoliz@gmail.com>
2023-05-13 12:52:57 -0700

    more updates

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -15,8 +15,12 @@ export PATH="$PATH:$HOME/bin/flutter/bin"
 
 # Ruby Gems
 export GEM_HOME=$HOME/.gem
-export PATH=$GEM_HOME/bin:$PATH
+# export PATH=$GEM_HOME/bin:$PATH
+export PATH="$PATH:$GEM_HOME/ruby/2.7.0/bin"
 
 # hashicorps
 export PATH="$PATH:$HOME/hashicorps/bin"
+
+# python
+export PATH="$PATH:/usr/local/opt/python@3.9/Frameworks/Python.framework/Versions/3.9/bin:$HOME/Library/Python/3.9/bin:$HOME/Library/Python/3.9/lib/python/site-packages"
 
